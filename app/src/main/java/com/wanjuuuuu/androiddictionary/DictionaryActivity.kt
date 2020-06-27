c5332783e51f191378b84e03b0f26da8b4097dfd
Wanjuuuuu <makarong5798@gmail.com>
2020-06-27 22:56:10 +0900

    Rename MainActivity into DictionaryActivity

---
+++ app/src/main/java/com/wanjuuuuu/androiddictionary/DictionaryActivity.kt
@@ -0,0 +1,16 @@
+package com.wanjuuuuu.androiddictionary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.wanjuuuuu.androiddictionary.databinding.ActivityDictionaryBinding
+
+class DictionaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        DataBindingUtil.setContentView<ActivityDictionaryBinding>(
+            this,
+            R.layout.activity_dictionary
+        )
+    }
+}
