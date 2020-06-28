9cf1ca6299489cca709225b23f8ead6c9bb45836
Wanjuuuuu <makarong5798@gmail.com>
2020-06-28 17:30:42 +0900

    Add modifyDateString property to Term

--- app/src/main/java/com/wanjuuuuu/androiddictionary/data/Term.kt
+++ app/src/main/java/com/wanjuuuuu/androiddictionary/data/Term.kt
@@ -1,6 +1,22 @@
 package com.wanjuuuuu.androiddictionary.data
 
+import java.text.SimpleDateFormat
+import java.util.*
+
 data class Term(val name: String, val url: String) {
+
+    companion object {
+        val timeZone = TimeZone.getTimeZone("Asia/Seoul")
+        val dateFormat = SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분", Locale.KOREA)
+    }
+
     var description: String = ""
-    var date: Long = System.currentTimeMillis();
+    var modifyTime: Long = System.currentTimeMillis()
+    val modifyTimeString: String
+        get() {
+            val calendar = Calendar.getInstance()
+            calendar.timeZone = timeZone
+            calendar.timeInMillis = modifyTime
+            return dateFormat.format(calendar.time)
+        }
 }
