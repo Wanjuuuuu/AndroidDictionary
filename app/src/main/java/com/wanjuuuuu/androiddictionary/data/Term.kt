f26d2df883d8faf83004ecf2550aa117a36cb441
Wanjuuuuu <makarong5798@gmail.com>
2020-07-04 11:36:46 +0900

    Add private visiblity modifier to companion object only used in class

--- app/src/main/java/com/wanjuuuuu/androiddictionary/data/Term.kt
+++ app/src/main/java/com/wanjuuuuu/androiddictionary/data/Term.kt
@@ -6,8 +6,8 @@ import java.util.*
 data class Term(val name: String, val url: String) {
 
     companion object {
-        val timeZone = TimeZone.getTimeZone("Asia/Seoul")
-        val dateFormat = SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분", Locale.KOREA)
+        private val timeZone = TimeZone.getTimeZone("Asia/Seoul")
+        private val dateFormat = SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분", Locale.KOREA)
     }
 
     var description: String = ""
