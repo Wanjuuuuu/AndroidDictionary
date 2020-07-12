e34fac4a283ea55d1ffb37cc02c5060dddf41595
Wanjuuuuu <makarong5798@gmail.com>
2020-07-12 21:27:46 +0900

    Extract TermScrapper from crawling logic at TermRepository

--- app/src/main/java/com/wanjuuuuu/androiddictionary/data/TermRepository.kt
+++ app/src/main/java/com/wanjuuuuu/androiddictionary/data/TermRepository.kt
@@ -5,20 +5,14 @@ import android.util.Log
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.google.gson.stream.JsonReader
-import com.wanjuuuuu.androiddictionary.utils.ANDROID_REFERENCE_BASE_URL
 import com.wanjuuuuu.androiddictionary.utils.TERM_DATA_FILENAME
 import kotlinx.coroutines.*
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Element
-import org.jsoup.select.Elements
 import java.lang.Exception
 
 class TermRepository(private val coroutineScope: CoroutineScope) {
 
     companion object {
         private const val TAG = "TermRepository"
-        private const val SELECTOR =
-            "#jd-content > p, #jd-content > h3, #jd-content > ol, #jd-content > ul"
     }
 
     fun getTerms(context: Context): List<Term> {
@@ -36,60 +30,13 @@ class TermRepository(private val coroutineScope: CoroutineScope) {
         }
     }
 
-    suspend fun getTerm(term: Term): Term {
-        val description = getDescription(term.url).await()
-        term.description = description
-        term.modifyTime = System.currentTimeMillis()
-        Log.d(TAG, "$term, ${term.modifyTime} ${term.description}")
+    fun getTerm(term: Term): Term {
+        coroutineScope.launch(Dispatchers.IO) {
+            val description = TermScrapper().getDescription(term.url)
+            term.description = description
+            term.modifyTime = System.currentTimeMillis()
+            Log.d(TAG, "$term, ${term.modifyTime} ${term.description}")
+        }
         return term
-    }
-
-    private fun getDescription(url: String): Deferred<String> =
-        coroutineScope.async(Dispatchers.IO) {
-            var description = ""
-            try {
-                val document = Jsoup.connect("$ANDROID_REFERENCE_BASE_URL$url").get()
-                val elements = document.body().select(SELECTOR)
-                description = parseDescription(elements)
-                Log.d(TAG, description)
-            } catch (e: Exception) {
-                Log.e(TAG, "cannot parse : $e")
-            }
-            description
-        }
-
-    private fun parseDescription(elements: Elements): String {
-        val stringBuilder = StringBuilder()
-        for ((index, element) in elements.withIndex()) {
-            parseElement(stringBuilder, element)
-            if (index != elements.lastIndex) {
-                stringBuilder.appendln()
-            }
-        }
-        return stringBuilder.toString()
-    }
-
-    private fun parseElement(stringBuilder: StringBuilder, element: Element) {
-        when {
-            element.`is`("p") -> {
-                stringBuilder.append(" ")
-                stringBuilder.appendln(element.text())
-            }
-            element.`is`("ol") -> parseListElement(stringBuilder, element, true)
-            element.`is`("ul") -> parseListElement(stringBuilder, element, false)
-            else -> stringBuilder.appendln(element.text())
-        }
-    }
-
-    private fun parseListElement(
-        stringBuilder: StringBuilder,
-        listElement: Element,
-        isOrdered: Boolean
-    ) {
-        val elements = listElement.select("li")
-        for ((index, element) in elements.withIndex()) {
-            stringBuilder.append(if (isOrdered) "\t${index + 1}. " else "\t- ")
-            stringBuilder.appendln(element.text())
-        }
     }
 }
---
+++ app/src/main/java/com/wanjuuuuu/androiddictionary/data/TermScrapper.kt
@@ -0,0 +1,65 @@
+package com.wanjuuuuu.androiddictionary.data
+
+import android.util.Log
+import com.wanjuuuuu.androiddictionary.utils.ANDROID_REFERENCE_BASE_URL
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+import java.lang.Exception
+
+class TermScrapper {
+
+    companion object {
+        private const val TAG = "TermScrapper"
+        private const val SELECTOR =
+            "#jd-content > p, #jd-content > h3, #jd-content > ol, #jd-content > ul"
+    }
+
+    fun getDescription(url: String): String {
+        var description = ""
+        try {
+            val document = Jsoup.connect("$ANDROID_REFERENCE_BASE_URL$url").get()
+            val elements = document.body().select(SELECTOR)
+            description = parseDescription(elements)
+            Log.d(TAG, description)
+        } catch (e: Exception) {
+            Log.e(TAG, "cannot parse : $e")
+        }
+        return description
+    }
+
+    private fun parseDescription(elements: Elements): String {
+        val stringBuilder = StringBuilder()
+        for ((index, element) in elements.withIndex()) {
+            parseElement(stringBuilder, element)
+            if (index != elements.lastIndex) {
+                stringBuilder.appendln()
+            }
+        }
+        return stringBuilder.toString()
+    }
+
+    private fun parseElement(stringBuilder: StringBuilder, element: Element) {
+        when {
+            element.`is`("p") -> {
+                stringBuilder.append(" ")
+                stringBuilder.appendln(element.text())
+            }
+            element.`is`("ol") -> parseListElement(stringBuilder, element, true)
+            element.`is`("ul") -> parseListElement(stringBuilder, element, false)
+            else -> stringBuilder.appendln(element.text())
+        }
+    }
+
+    private fun parseListElement(
+        stringBuilder: StringBuilder,
+        listElement: Element,
+        isOrdered: Boolean
+    ) {
+        val elements = listElement.select("li")
+        for ((index, element) in elements.withIndex()) {
+            stringBuilder.append(if (isOrdered) "\t${index + 1}. " else "\t- ")
+            stringBuilder.appendln(element.text())
+        }
+    }
+}
