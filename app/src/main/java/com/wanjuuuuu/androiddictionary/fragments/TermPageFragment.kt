4c0d2c98d96531279bd5c5aaeef97452530ef271
Wanjuuuuu <makarong5798@gmail.com>
2020-10-14 22:32:39 +0900

    Allow loadUrl only for developer reference, otherwise use Browser

--- app/src/main/java/com/wanjuuuuu/androiddictionary/fragments/TermPageFragment.kt
+++ app/src/main/java/com/wanjuuuuu/androiddictionary/fragments/TermPageFragment.kt
@@ -1,9 +1,13 @@
 package com.wanjuuuuu.androiddictionary.fragments
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.wanjuuuuu.androiddictionary.databinding.FragmentTermPageBinding
@@ -12,6 +16,30 @@ import com.wanjuuuuu.androiddictionary.utils.ANDROID_REFERENCE_BASE_URL
 class TermPageFragment : Fragment() {
 
     private val args: TermPageFragmentArgs by navArgs()
+    private val webViewUrlHandler = object : WebViewClient() {
+        override fun shouldOverrideUrlLoading(
+            view: WebView?,
+            request: WebResourceRequest?
+        ): Boolean {
+            val uri = request?.run { Uri.parse(url.toString()) }
+            return routeToBrowser(uri)
+        }
+
+        override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+            val uri = url?.let { Uri.parse(it) }
+            return routeToBrowser(uri)
+        }
+    }
+
+    private fun routeToBrowser(uri: Uri?): Boolean {
+        uri?.run {
+            if (host == ANDROID_REFERENCE_BASE_URL) return false
+            Intent(Intent.ACTION_VIEW, uri).apply {
+                startActivity(this)
+            }
+        }
+        return true
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,8 +47,18 @@ class TermPageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentTermPageBinding.inflate(inflater, container, false).apply {
-            termPage.loadUrl("$ANDROID_REFERENCE_BASE_URL${args.termUrl}")
+            setUpTermPage(termPage)
         }
         return binding.root
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setUpTermPage(webView: WebView) {
+        with(webView) {
+            settings.setSupportZoom(false)
+            settings.javaScriptEnabled = true
+            webViewClient = webViewUrlHandler
+            loadUrl("$ANDROID_REFERENCE_BASE_URL${args.termUrl}")
+        }
     }
 }
