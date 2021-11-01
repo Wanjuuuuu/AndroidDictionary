97488285ff7890f6e7908c60ffa0af0c4e01e174
Wanjuuuuu <makarong5798@gmail.com>
2021-11-01 23:46:33 +0900

    Add db migration test

--- app/src/main/java/com/wanjuuuuu/androiddictionary/data/AppDatabase.kt
+++ app/src/main/java/com/wanjuuuuu/androiddictionary/data/AppDatabase.kt
@@ -19,7 +19,7 @@ abstract class AppDatabase : RoomDatabase() {
         @Volatile
         private var instance: AppDatabase? = null
 
-        private val MIGRATION_1_2 = object : Migration(1, 2) {
+        val MIGRATION_1_2 = object : Migration(1, 2) {
             override fun migrate(database: SupportSQLiteDatabase) {
                 with(database) {
                     execSQL("CREATE TABLE new_terms (name TEXT NOT NULL, category TEXT NOT NULL, id Integer NOT NULL, description TEXT, scrapedTime Integer NOT NULL, bookmarked Integer NOT NULL, PRIMARY KEY(`id`)) ")
