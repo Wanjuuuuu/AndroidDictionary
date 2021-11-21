73f21787eecbd0aa5fd33e3d248b002ca3af0a42
Wanjuuuuu <makarong5798@gmail.com>
2021-11-21 20:29:55 +0900

    Update flow only really updated

--- app/src/main/java/com/wanjuuuuu/androiddictionary/data/TermDao.kt
+++ app/src/main/java/com/wanjuuuuu/androiddictionary/data/TermDao.kt
@@ -3,34 +3,41 @@ package com.wanjuuuuu.androiddictionary.data
 import androidx.lifecycle.LiveData
 import androidx.room.*
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
 
 @Dao
-interface TermDao {
+abstract class TermDao {
 
-    @get:Query("select id, name, category, bookmarked from terms order by id")
     val allTerms: Flow<List<TermListItem>>
+        get() = allTerms().distinctUntilChanged()
 
-    @get:Query("select id, name, category, bookmarked from terms where bookmarked = 1 order by id")
     val bookmarkedTerms: Flow<List<TermListItem>>
+        get() = bookmarkedTerms().distinctUntilChanged()
+
+    @Query("select id, name, category, bookmarked from terms order by id")
+    protected abstract fun allTerms(): Flow<List<TermListItem>>
+
+    @Query("select id, name, category, bookmarked from terms where bookmarked = 1 order by id")
+    protected abstract fun bookmarkedTerms(): Flow<List<TermListItem>>
 
     @Query("select * from terms where id = :id")
-    fun getTerm(id: Long): LiveData<Term>
+    abstract fun getTerm(id: Long): LiveData<Term>
 
     @Query("select * from terms where id = :id")
-    suspend fun getNaiveTerm(id: Long): Term
+    abstract suspend fun getNaiveTerm(id: Long): Term
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertTerms(terms: List<Term>)
+    abstract suspend fun insertTerms(terms: List<Term>)
 
     @Query("update terms set description = :description, scrapedTime = :scrapedTime where id = :id")
-    suspend fun updateTerm(id: Long, description: String, scrapedTime: Long)
+    abstract suspend fun updateTerm(id: Long, description: String, scrapedTime: Long)
 
     @Query("update terms set bookmarked = :bookmarked where id = :id")
-    suspend fun updateTermBookmarked(id: Long, bookmarked: Boolean)
+    abstract suspend fun updateTermBookmarked(id: Long, bookmarked: Boolean)
 
     @Delete
-    fun deleteTerm(term: Term)
+    abstract fun deleteTerm(term: Term)
 
     @Delete
-    fun deleteTerms(term: Term)
+    abstract fun deleteTerms(term: Term)
 }
